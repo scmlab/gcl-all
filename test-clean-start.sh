597d19bdcdcf862c90df234bdc950451ecce4ca5
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2025-06-25 15:49:26 +0800

    add test-clean-start.sh to test in a complete clean env

---
+++ test-clean-start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
+
+IMG="ghcr.io/lcamel/gcl-all-builder-deps-bin"
+docker ps -a --filter "ancestor=$IMG:latest" -q | xargs -r docker rm -f
+docker tag "$IMG:latest" "$IMG:backup"
+docker rmi "$IMG:latest"
+
+D=/tmp/tmp-gcl-all
+rm -fR "$D" && mkdir -p "$D"
+code --user-data-dir "$D/u" --extensions-dir "$D/e" .
