58f9972a83f162bc42d1b3685443193f6d871148
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2025-12-19 12:25:54 +0800

    make "gcl/update" message types explicit (#45)

--- gcl/src/Server/Notification/Update.hs
+++ gcl/src/Server/Notification/Update.hs
@@ -1,24 +1,13 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE InstanceSigs #-}
-{-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 
 module Server.Notification.Update where
 
-import Data.Aeson (object, (.=))
 import qualified Data.Aeson as JSON
 import Data.Proxy (Proxy (Proxy))
-import GCL.Predicate (Origin (..), PO (..), Spec (..))
-import GCL.WP.Types (StructWarning (..))
-import Pretty.Predicate ()
-import Pretty.Typed ()
-import Prettyprinter
-import Server.Monad (FileState (..), ServerM, loadFileState)
+import Server.Monad (ServerM, loadFileState)
 import qualified Server.Monad as Server
-import Server.SrcLoc (toLSPRange)
+import qualified Server.ToClient as ToClient
 
 sendUpdateNotification :: FilePath -> ServerM ()
 sendUpdateNotification filePath = do
@@ -26,94 +15,6 @@ sendUpdateNotification filePath = do
   case maybeFileState of
     Nothing -> return ()
     Just fileState -> do
-      let json :: JSON.Value = makeUpdateNotificationJson filePath fileState
+      let clientFileState = ToClient.convertFileState filePath fileState
+      let json = JSON.toJSON clientFileState
       Server.sendCustomNotification (Proxy @"gcl/update") json
-
-makeUpdateNotificationJson :: FilePath -> FileState -> JSON.Value
-makeUpdateNotificationJson filePath FileState {specifications, proofObligations, warnings} =
-  JSON.object
-    [ "filePath" .= JSON.toJSON filePath,
-      "specs" .= JSON.toJSON (map snd specifications),
-      "pos" .= JSON.toJSON (map snd proofObligations),
-      "warnings" .= JSON.toJSON (map snd warnings)
-    ]
-
-instance JSON.ToJSON Spec where
-  toJSON :: Spec -> JSON.Value
-  toJSON Specification {..} =
-    object
-      [ "id" .= JSON.toJSON (show specID),
-        "preCondition" .= JSON.toJSON (show $ pretty specPreCond),
-        "postCondition" .= JSON.toJSON (show $ pretty specPostCond),
-        "specRange" .= JSON.toJSON (toLSPRange specRange)
-      ]
-
-instance JSON.ToJSON StructWarning where
-  toJSON :: StructWarning -> JSON.Value
-  toJSON (MissingBound range) =
-    object
-      [ "tag" .= JSON.String "MissingBound",
-        "range" .= JSON.toJSON (toLSPRange range)
-      ]
-
-instance JSON.ToJSON PO where
-  toJSON :: PO -> JSON.Value
-  toJSON PO {..} =
-    object
-      [ "assumption" .= JSON.toJSON (show $ pretty poPre),
-        "goal" .= JSON.toJSON (show $ pretty poPost),
-        "hash" .= JSON.toJSON poAnchorHash,
-        "proofLocation" .= case poAnchorRange of
-          Nothing -> JSON.Null
-          Just range -> JSON.toJSON (toLSPRange range),
-        "origin" .= JSON.toJSON poOrigin
-      ]
-
---         tag: "Abort" | "Skip" | "Spec" | "Assignment" | "Conditional" | "Loop invariant" | "Loop termination";
-instance JSON.ToJSON Origin where
-  toJSON :: Origin -> JSON.Value
-  toJSON (AtAbort loc) =
-    object
-      [ "tag" .= JSON.String "Abort",
-        "location" .= fmap toLSPRange loc
-      ]
-  toJSON (AtSkip loc) =
-    object
-      [ "tag" .= JSON.String "Skip",
-        "location" .= fmap toLSPRange loc
-      ]
-  toJSON (AtSpec loc) =
-    object
-      [ "tag" .= JSON.String "Spec",
-        "location" .= fmap toLSPRange loc
-      ]
-  toJSON (AtAssignment loc) =
-    object
-      [ "tag" .= JSON.String "Assignment",
-        "location" .= fmap toLSPRange loc
-      ]
-  toJSON (AtAssertion loc) =
-    object
-      [ "tag" .= JSON.String "Assertion",
-        "location" .= fmap toLSPRange loc
-      ]
-  toJSON (AtIf loc) =
-    object
-      [ "tag" .= JSON.String "Conditional",
-        "location" .= fmap toLSPRange loc
-      ]
-  toJSON (AtLoop loc) =
-    object
-      [ "tag" .= JSON.String "Loop invariant",
-        "location" .= fmap toLSPRange loc
-      ]
-  toJSON (AtTermination loc) =
-    object
-      [ "tag" .= JSON.String "Loop termination",
-        "location" .= fmap toLSPRange loc
-      ]
-  toJSON Explain {originHeader, originRange} =
-    object
-      [ "tag" .= JSON.String originHeader,
-        "location" .= fmap toLSPRange originRange
-      ]
---
+++ gcl/src/Server/ToClient.hs
@@ -0,0 +1,217 @@
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+-- | This module defines types that match the client-side TypeScript types
+-- and provides conversion functions from server-side types to client types.
+-- The main purpose is to make the conversion from server to client explicit
+-- and handle the differences between server (1-based) and client (0-based) ranges.
+module Server.ToClient
+  ( FileState (..),
+    Specification (..),
+    ProofObligation (..),
+    StructWarning (..),
+    POOrigin (..),
+    convertFileState,
+  )
+where
+
+import Data.Aeson (object, (.=))
+import qualified Data.Aeson as JSON
+import qualified Data.Text as Text
+import qualified GCL.Predicate as GCL
+import qualified GCL.WP.Types as GCL
+import qualified Language.LSP.Protocol.Types as LSP
+import Pretty.Predicate ()
+import Pretty.Typed ()
+import Prettyprinter (Pretty (pretty))
+import qualified Server.Monad as Server
+import Server.SrcLoc (toLSPRange)
+
+-- | Client-side FileState type (matches TypeScript interface)
+data FileState = FileState
+  { filePath :: FilePath,
+    specs :: [Specification],
+    pos :: [ProofObligation],
+    warnings :: [StructWarning]
+  }
+  deriving (Show)
+
+-- | Client-side Specification type (matches TypeScript ISpecification)
+data Specification = Specification
+  { specId :: String,
+    preCondition :: String,
+    postCondition :: String,
+    specRange :: LSP.Range
+  }
+  deriving (Show)
+
+-- | Client-side ProofObligation type (matches TypeScript IProofObligation)
+data ProofObligation = ProofObligation
+  { assumption :: String,
+    goal :: String,
+    hash :: String,
+    proofLocation :: Maybe LSP.Range,
+    origin :: POOrigin
+  }
+  deriving (Show)
+
+-- | Client-side POOrigin type (matches TypeScript origin type)
+data POOrigin = POOrigin
+  { tag :: Text.Text,
+    location :: Maybe LSP.Range,
+    explanation :: Maybe String
+  }
+  deriving (Show)
+
+-- | Client-side StructWarning type (matches TypeScript IStructWarning)
+data StructWarning
+  = MissingBound LSP.Range
+  deriving (Show)
+
+-- | Convert server-side FileState to client-side FileState
+-- This function extracts only the fields needed by the client and
+-- converts 1-based server ranges to 0-based LSP ranges.
+convertFileState :: FilePath -> Server.FileState -> FileState
+convertFileState path serverFileState =
+  FileState
+    { filePath = path,
+      specs = map (convertSpec . Server.unversioned) (Server.specifications serverFileState),
+      pos = map (convertPO . Server.unversioned) (Server.proofObligations serverFileState),
+      warnings = map (convertWarning . Server.unversioned) (Server.warnings serverFileState)
+    }
+
+-- | Convert server-side Spec to client-side Specification
+convertSpec :: GCL.Spec -> Specification
+convertSpec (GCL.Specification {GCL.specID, GCL.specPreCond, GCL.specPostCond, GCL.specRange}) =
+  Specification
+    { specId = show specID,
+      preCondition = show $ pretty specPreCond,
+      postCondition = show $ pretty specPostCond,
+      specRange = toLSPRange specRange
+    }
+
+-- | Convert server-side PO to client-side ProofObligation
+convertPO :: GCL.PO -> ProofObligation
+convertPO (GCL.PO {GCL.poPre, GCL.poPost, GCL.poAnchorHash, GCL.poAnchorRange, GCL.poOrigin}) =
+  ProofObligation
+    { assumption = show $ pretty poPre,
+      goal = show $ pretty poPost,
+      hash = Text.unpack poAnchorHash,
+      proofLocation = fmap toLSPRange poAnchorRange,
+      origin = convertOrigin poOrigin
+    }
+
+-- | Convert server-side Origin to client-side POOrigin
+convertOrigin :: GCL.Origin -> POOrigin
+convertOrigin (GCL.AtAbort loc) =
+  POOrigin
+    { tag = "Abort",
+      location = fmap toLSPRange loc,
+      explanation = Nothing
+    }
+convertOrigin (GCL.AtSkip loc) =
+  POOrigin
+    { tag = "Skip",
+      location = fmap toLSPRange loc,
+      explanation = Nothing
+    }
+convertOrigin (GCL.AtSpec loc) =
+  POOrigin
+    { tag = "Spec",
+      location = fmap toLSPRange loc,
+      explanation = Nothing
+    }
+convertOrigin (GCL.AtAssignment loc) =
+  POOrigin
+    { tag = "Assignment",
+      location = fmap toLSPRange loc,
+      explanation = Nothing
+    }
+convertOrigin (GCL.AtAssertion loc) =
+  POOrigin
+    { tag = "Assertion",
+      location = fmap toLSPRange loc,
+      explanation = Nothing
+    }
+convertOrigin (GCL.AtIf loc) =
+  POOrigin
+    { tag = "Conditional",
+      location = fmap toLSPRange loc,
+      explanation = Nothing
+    }
+convertOrigin (GCL.AtLoop loc) =
+  POOrigin
+    { tag = "Loop invariant",
+      location = fmap toLSPRange loc,
+      explanation = Nothing
+    }
+convertOrigin (GCL.AtTermination loc) =
+  POOrigin
+    { tag = "Loop termination",
+      location = fmap toLSPRange loc,
+      explanation = Nothing
+    }
+convertOrigin (GCL.Explain {GCL.originHeader, GCL.originRange}) =
+  POOrigin
+    { tag = originHeader,
+      location = fmap toLSPRange originRange,
+      explanation = Nothing
+    }
+
+-- | Convert server-side StructWarning to client-side StructWarning
+convertWarning :: GCL.StructWarning -> StructWarning
+convertWarning (GCL.MissingBound range) = MissingBound (toLSPRange range)
+
+--------------------------------------------------------------------------------
+-- JSON instances for client types
+
+instance JSON.ToJSON FileState where
+  toJSON :: FileState -> JSON.Value
+  toJSON FileState {filePath, specs, pos, warnings} =
+    object
+      [ "filePath" .= JSON.toJSON filePath,
+        "specs" .= JSON.toJSON specs,
+        "pos" .= JSON.toJSON pos,
+        "warnings" .= JSON.toJSON warnings
+      ]
+
+instance JSON.ToJSON Specification where
+  toJSON :: Specification -> JSON.Value
+  toJSON Specification {specId, preCondition, postCondition, specRange} =
+    object
+      [ "id" .= JSON.toJSON specId,
+        "preCondition" .= JSON.toJSON preCondition,
+        "postCondition" .= JSON.toJSON postCondition,
+        "specRange" .= JSON.toJSON specRange
+      ]
+
+instance JSON.ToJSON ProofObligation where
+  toJSON :: ProofObligation -> JSON.Value
+  toJSON ProofObligation {assumption, goal, hash, proofLocation, origin} =
+    object
+      [ "assumption" .= JSON.toJSON assumption,
+        "goal" .= JSON.toJSON goal,
+        "hash" .= JSON.toJSON hash,
+        "proofLocation" .= case proofLocation of
+          Nothing -> JSON.Null
+          Just range -> JSON.toJSON range,
+        "origin" .= JSON.toJSON origin
+      ]
+
+instance JSON.ToJSON POOrigin where
+  toJSON :: POOrigin -> JSON.Value
+  toJSON POOrigin {tag, location, explanation} =
+    object
+      [ "tag" .= JSON.String tag,
+        "location" .= fmap JSON.toJSON location,
+        "explanation" .= fmap JSON.toJSON explanation
+      ]
+
+instance JSON.ToJSON StructWarning where
+  toJSON :: StructWarning -> JSON.Value
+  toJSON (MissingBound range) =
+    object
+      [ "tag" .= JSON.String "MissingBound",
+        "range" .= JSON.toJSON range
+      ]
