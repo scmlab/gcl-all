dea075a8c76872ea134b22393bfec8d490d40188
noobdy <htommyf12@gmail.com>
2026-02-10 16:53:50 +0800

    rename to range

--- gcl/src/GCL/Type2/ToTyped.hs
+++ gcl/src/GCL/Type2/ToTyped.hs
@@ -37,7 +37,7 @@ type DefnMap = Map A.Definition Scheme
 
 -- XXX: is checking duplicate definition required here?
 collectDefnToEnv :: A.Definition -> RSE Env Inference Env
-collectDefnToEnv (A.TypeDefn name args ctors _loc) = do
+collectDefnToEnv (A.TypeDefn name args ctors _range) = do
   let nameTy = A.TData name (maybeRangeOf name)
 
   let kind = foldr (\_ acc -> A.TType `typeToType` acc) A.TType args
@@ -91,7 +91,7 @@ class ToTyped a t | a -> t where
   toTyped :: a -> RSE Env Inference t
 
 instance ToTyped A.Program T.Program where
-  toTyped (A.Program defns decls exprs stmts loc) = do
+  toTyped (A.Program defns decls exprs stmts range) = do
     traceM $ "defns: " <> show (pretty defns)
     traceM $ "decls: " <> show (pretty decls)
     traceM $ "exprs: " <> show (pretty exprs)
@@ -139,7 +139,7 @@ instance ToTyped A.Program T.Program where
         decls
     typedExprs <- local (const newEnv) (mapM toTyped exprs)
     typedStmts <- local (const newEnv) (mapM toTyped stmts)
-    return $ T.Program typedDefns typedDecls typedExprs typedStmts loc
+    return $ T.Program typedDefns typedDecls typedExprs typedStmts range
 
 instance ToTyped A.Definition T.Definition where
   toTyped (A.TypeDefn name args ctors loc) = return $ T.TypeDefn name args (map toTypedTypeDefnCtor ctors) loc
@@ -150,32 +150,32 @@ toTypedTypeDefnCtor :: A.TypeDefnCtor -> T.TypeDefnCtor
 toTypedTypeDefnCtor (A.TypeDefnCtor name args) = T.TypeDefnCtor name args
 
 instance ToTyped A.Declaration T.Declaration where
-  toTyped (A.ConstDecl names ty prop loc) = do
+  toTyped (A.ConstDecl names ty prop range) = do
     -- TODO: some kind stuff
     case prop of
       Just p -> do
         p' <- toTyped p
-        return $ T.ConstDecl names ty (Just p') loc
+        return $ T.ConstDecl names ty (Just p') range
       Nothing ->
-        return $ T.ConstDecl names ty Nothing loc
-  toTyped (A.VarDecl names ty prop loc) = do
+        return $ T.ConstDecl names ty Nothing range
+  toTyped (A.VarDecl names ty prop range) = do
     -- TODO: some kind stuff
     case prop of
       Just p -> do
         p' <- toTyped p
-        return $ T.VarDecl names ty (Just p') loc
+        return $ T.VarDecl names ty (Just p') range
       Nothing ->
-        return $ T.VarDecl names ty Nothing loc
+        return $ T.VarDecl names ty Nothing range
 
 instance ToTyped A.Stmt T.Stmt where
-  toTyped (A.Skip loc) = return (T.Skip loc)
-  toTyped (A.Abort loc) = return (T.Abort loc)
-  toTyped (A.Assign names exprs loc) = toTypedAssign names exprs loc
-  toTyped (A.AAssign arr index expr loc) = toTypedAAssign arr index expr loc
-  toTyped (A.Assert expr loc) = toTypedAssert expr loc
-  toTyped (A.LoopInvariant e1 e2 loc) = toTypedLoopInvariant e1 e2 loc
-  toTyped (A.Do gds loc) = toTypedDo gds loc
-  toTyped (A.If gds loc) = toTypedIf gds loc
+  toTyped (A.Skip range) = return (T.Skip range)
+  toTyped (A.Abort range) = return (T.Abort range)
+  toTyped (A.Assign names exprs range) = toTypedAssign names exprs range
+  toTyped (A.AAssign arr index expr range) = toTypedAAssign arr index expr range
+  toTyped (A.Assert expr range) = toTypedAssert expr range
+  toTyped (A.LoopInvariant e1 e2 range) = toTypedLoopInvariant e1 e2 range
+  toTyped (A.Do gds range) = toTypedDo gds range
+  toTyped (A.If gds range) = toTypedIf gds range
   toTyped (A.Spec text range) = undefined -- XXX: what is this supposed to return?
   toTyped (A.Proof t1 t2 range) = return (T.Proof t1 t2 range)
   toTyped (A.Alloc var exprs range) = toTypedAlloc var exprs range
@@ -185,7 +185,7 @@ instance ToTyped A.Stmt T.Stmt where
   toTyped A.Block {} = undefined
 
 toTypedAssign :: [Name] -> [A.Expr] -> Maybe Range -> RSE Env Inference T.Stmt
-toTypedAssign names exprs loc
+toTypedAssign names exprs range
   | length names > length exprs = throwError $ RedundantNames (drop (length exprs) names)
   | length names < length exprs = throwError $ RedundantExprs (drop (length names) exprs)
   | otherwise = do
@@ -203,10 +203,10 @@ toTypedAssign names exprs loc
               toTyped expr
           )
           assignments
-      return $ T.Assign names typedExprs loc
+      return $ T.Assign names typedExprs range
 
 toTypedAAssign :: A.Expr -> A.Expr -> A.Expr -> Maybe Range -> RSE Env Inference T.Stmt
-toTypedAAssign arr index expr loc = do
+toTypedAAssign arr index expr range = do
   ftv <- freshTVar
   (sa, typedArr) <- typeCheck arr (typeInt `typeToType` ftv)
   (si, typedIndex) <- local (applySubstEnv sa) (typeCheck index typeInt)
@@ -218,18 +218,18 @@ toTypedAAssign arr index expr loc = do
           (applySubstExpr resultSubst typedArr)
           (applySubstExpr resultSubst typedIndex)
           (applySubstExpr resultSubst typedExpr)
-          loc
+          range
 
   return resultStmt
 
 toTypedAssert :: A.Expr -> Maybe Range -> RSE Env Inference T.Stmt
-toTypedAssert expr loc = do
+toTypedAssert expr range = do
   (s1, exprTy, typedExpr) <- infer expr
   s2 <- lift $ unify exprTy typeBool (maybeRangeOf expr)
-  return (T.Assert (applySubstExpr (s2 <> s1) typedExpr) loc)
+  return (T.Assert (applySubstExpr (s2 <> s1) typedExpr) range)
 
 toTypedLoopInvariant :: A.Expr -> A.Expr -> Maybe Range -> RSE Env Inference T.Stmt
-toTypedLoopInvariant e1 e2 loc = do
+toTypedLoopInvariant e1 e2 range = do
   (e1Subst, typedE1) <- typeCheck e1 typeBool
   (e2Subst, typedE2) <- local (applySubstEnv e1Subst) (typeCheck e2 typeInt)
   let resultSubst = e2Subst <> e1Subst
@@ -237,19 +237,19 @@ toTypedLoopInvariant e1 e2 loc = do
         T.LoopInvariant
           (applySubstExpr resultSubst typedE1)
           (applySubstExpr resultSubst typedE2)
-          loc
+          range
 
   return resultStmt
 
 toTypedDo :: [A.GdCmd] -> Maybe Range -> RSE Env Inference T.Stmt
-toTypedDo gds loc = do
+toTypedDo gds range = do
   typedGds <- mapM toTyped gds
-  return (T.Do typedGds loc)
+  return (T.Do typedGds range)
 
 toTypedIf :: [A.GdCmd] -> Maybe Range -> RSE Env Inference T.Stmt
-toTypedIf gds loc = do
+toTypedIf gds range = do
   typedGds <- mapM toTyped gds
-  return (T.If typedGds loc)
+  return (T.If typedGds range)
 
 toTypedAlloc :: Name -> [A.Expr] -> Maybe Range -> RSE Env Inference T.Stmt
 toTypedAlloc var exprs range = do
@@ -294,11 +294,11 @@ toTypedDispose expr range = do
   return $ T.Dispose typedExpr range
 
 instance ToTyped A.GdCmd T.GdCmd where
-  toTyped (A.GdCmd expr stmts loc) = do
+  toTyped (A.GdCmd expr stmts range) = do
     (exprSubst, typedExpr) <- typeCheck expr typeBool
     typedStmts <- mapM toTyped stmts
 
-    return (T.GdCmd (applySubstExpr exprSubst typedExpr) typedStmts loc)
+    return (T.GdCmd (applySubstExpr exprSubst typedExpr) typedStmts range)
 
 instance ToTyped A.Expr T.Expr where
   toTyped expr = do
