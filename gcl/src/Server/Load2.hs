2e0a3a67d27325f571230add163f6fa0d123a398
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2026-02-26 19:16:09 +0800

    add loadFull, diggedText, and applyEdits to Server.Load2

--- gcl/src/Server/Load2.hs
+++ gcl/src/Server/Load2.hs
@@ -1,13 +1,17 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Server.Load2 where
 
 import Data.IntMap (IntMap)
+import Data.List (sortBy)
+import Data.Ord (comparing)
 import Data.Text (Text)
+import qualified Data.Text as Text
 import Error (Error (..))
 import GCL.Predicate (Hole, PO, Spec)
-import GCL.Range (Range)
+import GCL.Range (Range, posCol, posLine, rangeEnd, rangeStart)
 import GCL.WP.Types (StructWarning)
 import qualified GCL.Type as TypeChecking
 import qualified GCL.WP as WP
@@ -126,3 +130,47 @@ collectHolesFromExpr _                         = []
 mapSepBy :: (a -> b) -> SepBy s a -> [b]
 mapSepBy f (Head c)       = [f c]
 mapSepBy f (Delim c _ cs) = f c : mapSepBy f cs
+
+--------------------------------------------------------------------------------
+-- Hole digging
+
+-- | Wrapper around load: if holes are found, dig them and call load again.
+-- Returns Left for real errors, Right (result, Nothing) if no holes were found,
+-- Right (result, Just (newSource, edits)) if holes were dug.
+loadFull :: FilePath -> Text -> Either Error (LoadResult, Maybe (Text, [(Range, Text)]))
+loadFull filePath source =
+  case load filePath source of
+    Left (HaltError err)   -> Left err
+    Left (HaltHoles holes) ->
+      let edits     = map (\(kind, range) -> (range, diggedText kind range)) holes
+          newSource = applyEdits source edits
+      in case load filePath newSource of
+           Left (HaltError err) -> Left err
+           Left (HaltHoles _)   -> Left (Others "Load2" "unexpected holes after digging" Nothing)
+           Right result         -> Right (result, Just (newSource, edits))
+    Right result -> Right (result, Nothing)
+
+-- | Replacement text for a hole, matching the behaviour of Server.Monad.digHoles.
+diggedText :: HoleKind -> Range -> Text
+diggedText StmtHole range = "[!\n" <> ind <> "\n" <> ind <> "!]"
+  where ind = Text.replicate (posCol (rangeStart range) - 1) " "
+diggedText ExprHole _ = "{! !}"
+
+-- | Apply a list of non-overlapping edits to source text.
+-- Edits must not overlap. They are applied using positions in the original source,
+-- processed from first to last.
+applyEdits :: Text -> [(Range, Text)] -> Text
+applyEdits source edits =
+  let ls       = Text.lines source
+      toOffset pos =
+        sum (map (\l -> Text.length l + 1) (take (posLine pos - 1) ls))
+          + (posCol pos - 1)
+      sorted   = sortBy (comparing (rangeStart . fst)) edits
+      go pos [] = Text.drop pos source
+      go pos ((range, replacement) : rest) =
+        let s = toOffset (rangeStart range)
+            e = toOffset (rangeEnd   range)
+        in Text.take (s - pos) (Text.drop pos source)
+             <> replacement
+             <> go e rest
+  in go 0 sorted
