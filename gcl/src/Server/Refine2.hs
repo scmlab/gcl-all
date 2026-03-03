c220bfb99ed682c08e41c78129c2d21c4e6f563a
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2026-03-03 16:43:24 +0800

    refactor Refine2 to simplify dig result types and apply formatting

--- gcl/src/Server/Refine2.hs
+++ gcl/src/Server/Refine2.hs
@@ -10,96 +10,89 @@ import qualified Data.Text as Text
 import Error (Error (..))
 import GCL.Common (Index, TypeInfo)
 import GCL.Predicate (Hole, InfMode (..), PO, Spec (..))
-import GCL.Range (Pos (..), Range (..), extractText, mkPos, mkRange, posCol, rangeStart)
+import GCL.Range (Pos (..), R (..), Range (..), extractText, mkPos, mkRange, rangeStart)
 import GCL.Type (runElaboration)
 import GCL.WP (collectStmtHoles, runWP, structStmts)
 import GCL.WP.Types (StructError, StructWarning)
 import Language.Lexer.Applicative (TokenStream (..))
-import Server.Handler.GCL.Refine () -- for Elab [A.Stmt] orphan instance
+import Server.Handler.GCL.Refine ()
+-- for Elab [A.Stmt] orphan instance
 import Server.Highlighting (collectHighlightingFromStmts)
 import Server.Hover (collectHoverInfoFromStmts)
-import Server.Load2 (DigResult, LoadResult (..), applyEdits, collectHolesFromStatements, diggedText)
-import Server.Monad (HoleKind (..))
-import qualified Language.LSP.Protocol.Types as LSP
-import qualified Syntax.Abstract as A
+import Server.Load2 (LoadResult (..), applyEdits, collectHolesFromStatements, diggedText)
 import qualified Syntax.Concrete as C
 import qualified Syntax.Concrete.Instances.ToAbstract as C
-import Syntax.Parser.Error (ParseError (..))
 import qualified Syntax.Parser as Parser
+import Syntax.Parser.Error (ParseError (..))
 import Syntax.Parser.Lexer (TokStream, scan)
-import GCL.Range (R (..))
 import qualified Syntax.Typed as T
-
---------------------------------------------------------------------------------
--- Types are reused from Server.Load2 (LoadResult, DigResult)
 
 --------------------------------------------------------------------------------
 -- Main pipeline
 
 -- | Full pipeline: validate spec → extract impl → parseAndDigFragment → loadConcreteFragment.
-refineAndDig :: FilePath -> Int -> Spec -> Text
-             -> Either Error (Maybe DigResult, LoadResult)
+refineAndDig :: FilePath -> Int -> Spec -> Text -> Either Error (Text, LoadResult)
 refineAndDig filePath idCount spec source = do
   let specRng = specRange spec
   when (isSingleLine specRng) $
     Left (Others "Refine" "Spec should have more than one line." (Just specRng))
   let implRange = shrinkRange 2 specRng
-      implText  = extractText implRange source
+      implText = extractText implRange source
   when (not (isFirstLineBlank implText)) $
     Left (Others "Refine" "The first line in the spec must be blank." (Just implRange))
   let implStart = rangeStart implRange
-  (maybeDig, stmts) <- parseAndDigFragment filePath implStart implText
+
+  (finalImplText, stmts) <- parseAndDigFragment filePath implStart implText
   result <- loadConcreteFragment (specTypeEnv spec) idCount spec stmts
-  return (maybeDig, result)
+  return (finalImplText, result)
 
 -- | Pipeline from concrete stmts: abstract → elaborate → sweep.
-loadConcreteFragment :: [(Index, TypeInfo)] -> Int -> Spec -> [C.Stmt]
-               -> Either Error LoadResult
+loadConcreteFragment :: [(Index, TypeInfo)] -> Int -> Spec -> [C.Stmt] -> Either Error LoadResult
 loadConcreteFragment typeEnv idCount spec stmts = do
   let abstract = C.runAbstractTransform stmts
   elaborated <- first TypeError $ runElaboration abstract typeEnv
   (pos, specs, holes, warnings, idCount') <-
     first StructError $ sweepFragment idCount spec elaborated
-  return LoadResult
-    { specifications   = specs
-    , holes            = holes
-    , proofObligations = pos
-    , warnings         = warnings
-    , idCount          = idCount'
-    , semanticTokens   = collectHighlightingFromStmts stmts
-    , definitionLinks  = mempty -- TODO: needs scope info from declarations
-    , hoverInfos       = collectHoverInfoFromStmts elaborated
-    }
+  return
+    LoadResult
+      { specifications = specs,
+        holes = holes,
+        proofObligations = pos,
+        warnings = warnings,
+        idCount = idCount',
+        semanticTokens = collectHighlightingFromStmts stmts,
+        definitionLinks = mempty, -- TODO: needs scope info from declarations
+        hoverInfos = collectHoverInfoFromStmts elaborated
+      }
 
 -- | Parse fragment and dig holes if needed.
 -- Internally uses relative positions (1,1) for hole digging,
 -- then absolute positions (fragmentStart) for the returned stmts.
-parseAndDigFragment :: FilePath -> Pos -> Text
-                    -> Either Error (Maybe DigResult, [C.Stmt])
+-- Returns (finalImplText, stmts).
+parseAndDigFragment :: FilePath -> Pos -> Text -> Either Error (Text, [C.Stmt])
 parseAndDigFragment filePath fragmentStart implText = do
+  -- TODO: reduce parsing
   (_, holes1) <- parseRelative implText
   case holes1 of
     [] -> do
       stmts <- parseAbsolute implText
-      Right (Nothing, stmts)
-    _  -> do
-      let (edits, newImplText) = replaceHoles implText holes1
-      (stmts2, holes2) <- parseAbsolute' newImplText
+      Right (implText, stmts)
+    _ -> do
+      let newImplText = digHoles implText holes1
+      stmts2 <- parseAbsolute newImplText
+      let holes2 = collectHolesFromStatements stmts2
       case holes2 of
-        [] -> Right (Just (edits, newImplText), stmts2)
-        _  -> Left (Others "Refine2" "unexpected holes after digging" Nothing)
+        [] -> Right (newImplText, stmts2)
+        _ -> Left (Others "Refine2" "unexpected holes after digging" Nothing)
   where
     parseRelative src = do
       stmts <- parseFragmentStmts filePath (mkPos 1 1) src
       return (stmts, collectHolesFromStatements stmts)
     parseAbsolute src =
       parseFragmentStmts filePath fragmentStart src
-    parseAbsolute' src = do
-      stmts <- parseAbsolute src
-      return (stmts, collectHolesFromStatements stmts)
-    replaceHoles src holeList = (edits, applyEdits src edits)
-      where
-        edits = map (\(kind, range) -> (range, diggedText kind range)) holeList
+    digHoles src holeList =
+      let edits = map (\(kind, range) -> (range, diggedText kind range)) holeList
+       in applyEdits src edits
 
 --------------------------------------------------------------------------------
 -- Fragment parsing
@@ -107,11 +100,11 @@ parseAndDigFragment filePath fragmentStart implText = do
 -- | Parse text as a list of statements with position translation.
 parseFragmentStmts :: FilePath -> Pos -> Text -> Either Error [C.Stmt]
 parseFragmentStmts filePath fragmentStart src =
-  let tokens  = scan filePath src
+  let tokens = scan filePath src
       tokens' = translateTokStream fragmentStart tokens
    in case Parser.parse Parser.statements filePath tokens' of
         Left (errors, logMsg) -> Left (ParseError (SyntacticError errors logMsg))
-        Right stmts           -> Right stmts
+        Right stmts -> Right stmts
 
 translateTokStream :: Pos -> TokStream -> TokStream
 translateTokStream start (TsToken (R range x) rest) =
@@ -125,19 +118,20 @@ translateTokenRange start (Range left right) =
 
 translatePos :: Pos -> Pos -> Pos
 translatePos (Pos lineStart colStart) (Pos lineOff colOff) =
-  mkPos (lineStart + lineOff - 1)
-        (if lineOff == 1 then colStart + colOff - 1 else colOff)
+  mkPos
+    (lineStart + lineOff - 1)
+    (if lineOff == 1 then colStart + colOff - 1 else colOff)
 
 --------------------------------------------------------------------------------
 -- Sweep fragment
 
 -- | Sweep a fragment using the spec's pre/post conditions.
-sweepFragment :: Int -> Spec -> [T.Stmt]
-              -> Either StructError ([PO], [Spec], [Hole], [StructWarning], Int)
+sweepFragment :: Int -> Spec -> [T.Stmt] -> Either StructError ([PO], [Spec], [Hole], [StructWarning], Int)
 sweepFragment counter (Specification _ pre post _ _) impl =
   second
-    (\(_, counter', (pos, specs, sws, _redexes)) ->
-        (pos, specs, concatMap collectStmtHoles impl, sws, counter'))
+    ( \(_, counter', (pos, specs, sws, _redexes)) ->
+        (pos, specs, concatMap collectStmtHoles impl, sws, counter')
+    )
     $ runWP
       (structStmts Primary (pre, Nothing) impl post)
       (Map.empty, [])
