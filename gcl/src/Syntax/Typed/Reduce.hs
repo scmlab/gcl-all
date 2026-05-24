cffb545b79fe4d58e2bdf899bebcafb8b91a707a
Shin-Cheng Mu <scm@iis.sinica.edu.tw>
2026-05-24 23:30:57 +0800

    handling Chain and Case.

--- gcl/src/Syntax/Typed/Reduce.hs
+++ gcl/src/Syntax/Typed/Reduce.hs
@@ -4,9 +4,9 @@ import GCL.Common (Fresh(..))
 import Control.Arrow ((***))
 import Syntax.Typed.Types
 import Syntax.Common.Types (Name, Op, TypeOp, nameToText)
+import Syntax.Abstract.Types (Lit (..), Pattern (..))
 import Syntax.Substitution
 import Syntax.Typed.Instances.Substitution
-
 
 type Redex = [Int]  -- path to a redex
 
@@ -50,7 +50,7 @@ redexRT (Lit _ _ _ ) = leaf
 redexRT (Var _ _ _) = leaf
 redexRT (Const _ _ _) = leaf
 redexRT (Op _ _) = leaf
-redexRT (Chain _) = leaf  -- should look into Chain. Omit for now.
+redexRT (Chain ch) = Node False (redexRTChain ch)
 redexRT (App f@(Lam _ _ _ _) e _) = Node True [redexRT f, redexRT e]
 redexRT (App f@(Var _ _ _) e _) = Node True [leaf, redexRT e]
 redexRT (App f e _) = Node False [redexRT f, redexRT e]
@@ -69,6 +69,11 @@ redexRT (Subst e sb) =
      Node True (redexRT e : map (redexRT . snd) sb)
 redexRT (EHole {}) = leaf
 
+redexRTChain :: Chain -> [RT]
+redexRTChain (Pure e) = [redexRT e]
+redexRTChain (More ch _ _ e) = redexRT e : redexRTChain ch
+   -- counting from the rightmost expression
+   -- simply because it makes things easier.
 
 type RZ = ([Int], RT)   --- a "zipper" for RT
 
@@ -84,11 +89,12 @@ currentPath (p, _) = reverse p
 descend :: RZ -> [RZ]
 descend (p, Node _ ts) = zipWith (\i t -> (i:p, t)) [0..] ts
 
----
+-- reduce
+
 type Env = [(Name, Expr)]
 
-
 reduce :: Fresh m => Env -> Expr -> Redex -> m Expr
+reduce env (Chain ch) (i:p) = Chain <$> reduceChain env ch i p
 reduce env (App (Lam x _ bdy _) e _) [] = betaReduce x bdy e
 reduce env exp@(App (Var f _ _) e r) [] =
   maybe (return exp)
@@ -118,7 +124,8 @@ reduce env (ArrUpd a i e r) (0:p) =
 reduce env (ArrUpd a i e r) (1:p) = ArrUpd a <$> reduce env i p <*> pure e <*> pure r
 reduce env (ArrUpd a i e r) (2:p) = ArrUpd a i <$> reduce env e p <*> pure r
 
-reduce env (Case e cls r) [] = undefined -- TODO: pattern matching!
+reduce env expr@(Case e cls _) [] =
+     maybe expr id <$> reduceCase env e cls -- return expr unchanged if cannot reduce
 reduce env (Case e cls r) (0:p) =
   Case <$> reduce env e p <*> pure cls <*> pure r
 reduce env (Case e cls r) (n:p) =
@@ -139,28 +146,35 @@ reduceNth _ _ [] _ = error "shouldn't happen"
 reduceNth env 0 (e:es) p = (:es) <$> reduce env e p
 reduceNth env n (e:es) p = (e:) <$> reduceNth env (n-1) es p
 
-
 betaReduce :: Fresh m => Name -> Expr -> Expr -> m Expr
 betaReduce x bdy e = subst [(nameToText x, e)] bdy
 
-{-
+reduceChain :: Fresh m => Env -> Chain -> Int -> Redex -> m Chain
+reduceChain env (Pure e) 0 p = Pure <$> reduce env e p
+reduceChain env (More ch op t e) 0 p = More ch op t <$> reduce env e p
+reduceChain env (More ch op t e) i p =
+    (\ch' -> More ch' op t e) <$> reduceChain env ch (i-1) p
+reduceChain _ _ _ _ = error "shouldn't happen (reduceChain)"
 
-data Expr
-  = Lit Lit Type (Maybe Range)
-  | Var Name Type (Maybe Range)
-  | Const Name Type (Maybe Range)
-  | Op Op Type
-  | Chain Chain
-  | App Expr Expr (Maybe Range)
-  | Lam Name Type Expr (Maybe Range)
-  | Tuple [Expr] -- for internal use
-  | OutT Int Expr -- for internal use
-  | Quant Expr [Name] Expr Expr (Maybe Range)
-  | ArrIdx Expr Expr (Maybe Range)
-  | ArrUpd Expr Expr Expr (Maybe Range)
-  | Case Expr [CaseClause] (Maybe Range)
-  | Subst Expr [(Name, Expr)]
-  | EHole Text Int Type Range Env
-  deriving (Eq, Show)
+reduceCase :: Fresh m => Env -> Expr -> [CaseClause] -> m (Maybe Expr)
+reduceCase env e [] = return Nothing
+reduceCase env e (CaseClause ptn rhs : cls) = do
+   case matchPattern e ptn of
+     Just subs -> Just <$> subst subs rhs
+     Nothing -> reduceCase env e cls
 
--}
+matchPattern :: Expr -> Pattern -> Maybe (Subst Expr)
+matchPattern (Lit l _ _) (PattLit l') | l == l' = Just []
+matchPattern e (PattBinder v) = Just [(nameToText v, e)]
+matchPattern _ (PattWildcard _) = Just []
+matchPattern (Tuple es) (PattTuple ps) | length es == length ps =
+     concat <$> joinMaybe (zipWith matchPattern es ps)
+-- matchPattern e (PattConstructor c ps)
+     -- todo: SCM: to be implemented!
+matchPattern _ _ = Nothing
+
+
+joinMaybe :: [Maybe a] -> Maybe [a]
+joinMaybe [] = Just []
+joinMaybe (Nothing : _) = Nothing
+joinMaybe (Just x : xs) = (x:) <$> joinMaybe xs
