e26deea094056ea26394889391881e7d2b1d5bb4
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2026-06-08 14:23:42 +0800

    refactor: consolidate setFileState and sendFileState into setAndSendFileState, extract sendRefreshes

--- gcl/src/Server/Notification/Update.hs
+++ gcl/src/Server/Notification/Update.hs
@@ -4,17 +4,17 @@
 module Server.Notification.Update where
 
 import Data.Proxy (Proxy (Proxy))
-import Server.Monad (FileState, ServerM, sendInlayHintRefresh, sendSemanticTokensRefresh)
+import Server.Monad (FileState, ServerM, sendInlayHintRefresh, sendSemanticTokensRefresh, setFileState)
 import qualified Server.Monad as Server
 import qualified Server.ToClient as ToClient
 
-sendFileState :: FilePath -> FileState -> ServerM ()
-sendFileState filePath fs = do
+setAndSendFileState :: FilePath -> FileState -> ServerM ()
+setAndSendFileState filePath fs = do
+  setFileState filePath fs
   let json = ToClient.toClientFileStateJSON filePath fs
   Server.sendCustomNotification (Proxy @"gcl/update") json
 
-sendFileStateWithRefresh :: FilePath -> FileState -> ServerM ()
-sendFileStateWithRefresh filePath fs = do
-  sendFileState filePath fs
+sendRefreshes :: ServerM ()
+sendRefreshes = do
   sendSemanticTokensRefresh
   sendInlayHintRefresh
